a326c752ad76c1965c90a03e422712970f4e8a12
yama-choki <lovepingpong0@gmail.com>
2022-10-31 10:01:40 +0900

    教材97　RestFulなコントローラー　様々な機能（CRUDなど）が雛形として用意されているモノ

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,8 @@ use App\Http\Controllers\TestController;
 */
 
 Route::get('tests/test', [ TestController::class, 'index']);
+
+// Route::resource('contacts', ContactFormController::class);
 
 Route::get('/', function () {
     return view('welcome');
