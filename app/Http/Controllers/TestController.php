5c9874989bd666d4b34cd24f6658d97bf7de2bed
yama-choki <lovepingpong0@gmail.com>
2022-10-31 09:35:35 +0900

    教材127、94　githubからファイルコピー、環境構築、フォーム用のDB作成

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        // Eloquent(エロクアント）
+        $values = Test::all();
+
+        $conunt = Test::count();
+
+        $firest = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb');
+
+        // クエリビルダ
+        $queryBilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $conunt, $firest, $whereBBB, $queryBilder);
+
+        return view('tests.test', compact('values'));
+        // 👆のクラスはFacadesの機能を使っているのを覚えておく！
+    }
+}
+
---
+++ database/migrations/2022_10_29_234453_create_tests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->timestamps();
+            //教材80でLaravel tinkerを実施　ターミナルで対話形式でDBを操作したり、閲覧したりできる
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+};
